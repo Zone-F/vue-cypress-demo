9e4da91235ac4abea7f907de53636d1ce5389025
zone98f <1191066968@qq.com>
2023-01-03 12:08:45 +0800

    chorm:修改BaseUrl配置

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,6 +1,5 @@
 describe('首页', () => {
-  it('是否可访问', () => {
-    cy.visit('http://192.168.31.229:8080/');
-    // cy.visit('http://localhost:8080/');
+  it.only('是否可访问', () => {
+    cy.visit('/');
   })
 })
